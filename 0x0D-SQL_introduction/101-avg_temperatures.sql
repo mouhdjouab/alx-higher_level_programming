f90e2a890d21e55bd9979f8268f0656525a1f592
mouhdjouab <mouh.bw@gmail.com>
2023-12-12 18:17:14 -0800

    done

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Display average temperature in Fahreheit by city ordered by temperature in descending order
+SELECT city, AVG(value) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY AVG(value) desc;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Display the highest temperature for 3 cities between July and August ordered by temperature
+SELECT city, AVG(value) AS avg_temp FROM `temperatures` WHERE `month` BETWEEN 7 AND 8 GROUP BY `city` ORDER BY AVG(value) desc LIMIT 3;
