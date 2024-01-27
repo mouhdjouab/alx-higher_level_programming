3263751e0d8c1ec9a038fa0a781222d82daa14e3
root <root@MOUH-213>
2024-01-27 02:19:31 +0100

    3 done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+# Sends a POST request to a given URL with a given email.
 
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
+
 import sys
 import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
+    vals = {"email": sys.argv[2]}
+    donne = urllib.parse.urlencode(vals).encode("ascii")
+
+    req = urllib.request.Request(sys.argv[1], donne)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python3
-# script that takes in a URL, sends a request to the URL and displays the
-# body of the response (decoded in utf-8)
-import sys
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
 
+#!/usr/bin/python3
+# Sends a request to a given URL and displays the response body.
+
+
+import sys
+import urllib.error
+import urllib.request
+
+
+url = sys.argv[1]
+
+req = urllib.request.Request(url)
 try:
-    with urlopen(Request(sys.argv[1])) as response:
-        body = response.read().decode("utf-8")
-except HTTPError as e:
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("ascii"))
+except urllib.error.HTTPError as e:
     print("Error code: {}".format(e.code))
