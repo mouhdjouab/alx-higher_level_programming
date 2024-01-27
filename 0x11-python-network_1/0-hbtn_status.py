aba7b1ebb33cd583477d834bf427333c72b8a967
root <root@MOUH-213>
2024-01-27 02:06:54 +0100

    3 task done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# script to make request with urlib library
+
+from urllib.request import Request,urlopen
+with urlopen(Request("https://alx-intranet.hbtn.io/status")) as response:
+    body=response.read()
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+from sys import argv
+from urllib.request import Request,urlopen
+
+if len(argv)==2:
+    req=Request(f"{argv[1]}")
+    with urlopen(req) as response:
+        item=response.headers
+        print(item["X-Request-Id"])
+else:
+    print(" Please enter the URL")
