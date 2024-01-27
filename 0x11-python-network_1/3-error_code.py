b618d488f244496597fd8be9aa0c998b261c9ebf
root <root@MOUH-213>
2024-01-27 02:21:59 +0100

    3 done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Sends a request to a given URL
  displays the response body.
-
 """
 import sys
 import urllib.error
 import urllib.request
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("ascii"))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
