b186ed80ec5ee0e47f5ad2f856694f0639f3a673
root <root@MOUH-213>
2024-01-28 20:53:44 +0100

    a

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+ Sends a POST request to a given URL with a given email.
+
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url=sys.argv[1]
+    values={"email":sys.argv[2]}
+    req=requests.get(url,params=values)
+    print(req.text)
