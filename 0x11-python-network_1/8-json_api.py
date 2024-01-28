b15c0502752e41e09b125466eda4d2c3350f8509
root <root@MOUH-213>
2024-01-28 21:12:36 +0100

    a

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-    Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 
 """
 import sys
 import requests
+
+
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    values = {}
-    values = {"q": sys.argv[1]
-              } if sys.argv[1] is not None else values["q"] == ""
-    req = requests.post(url, data=values)
+
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    val = {"q": letter}
+
+    reqes = requests.post("http://0.0.0.0:5000/search_user", data=val)
     try:
-        response = req.json()
-        if response == {}:
+        repons = reqes.json()
+        if repons == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(repons.get("id"), repons.get("name")))
     except ValueError:
         print("Not a valid JSON")
