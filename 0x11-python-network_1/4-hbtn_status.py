e830c8f0d944d04bff226765d3ae16907db10c92
root <root@MOUH-213>
2024-01-28 20:38:28 +0100

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,8 @@
 """Fetch https://intranet.hbtn.io/status."""
 import requests
 if __name__ == "__main__":
-    respone=requests.get("https://alx-intranet.hbtn.io/status")
+    respone = requests.get("https://alx-intranet.hbtn.io/status")
 
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(respone.text)))
+    print("\t- content: {}".format(respone.text))
