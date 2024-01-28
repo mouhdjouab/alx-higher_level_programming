1a338d9b88c1af7d5bd418ab4bf774a7a8c44fda
root <root@MOUH-213>
2024-01-28 21:25:39 +0100

    a

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Uses the GitHub API to display a GitHub ID based on given credentials.
+
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    autn = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=autn)
+    print(req.json().get("id"))
