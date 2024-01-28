d2476a35cc93f0a0bd416d1c861b6cd495ff2fdb
root <root@MOUH-213>
2024-01-28 20:55:17 +0100

    a

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {"email": sys.argv[2]}
-    req = requests.get(url, params=values)
+    req = requests.post(url, data=values)
     print(req.text)
